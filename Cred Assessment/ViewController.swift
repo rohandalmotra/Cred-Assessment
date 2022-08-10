116794b9df60f530c0e0fe19e8bc1a1475a79455
Rohan Dalmotra <rohandalmotra1@gmail.com>
2022-08-10 10:59:17 +0530

    Made launch screen animation

---
+++ Cred Assessment/ViewController.swift
@@ -0,0 +1,27 @@
+import UIKit
+
+class ViewController: UIViewController {
+    @IBOutlet weak var logoImage: UIImageView!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        DispatchQueue.main.asyncAfter(deadline: .now()+0.5, execute: {
+            self.animate()
+        })
+    }
+    
+    
+    func animate(){
+        UIImageView.animate(withDuration: 0.75, animations: {
+            self.logoImage.frame.origin.y = self.view.frame.maxY
+        })
+        
+        UIImageView.animate(withDuration: 1, animations: {
+            self.logoImage.alpha = 0
+        }, completion: { done in
+            self.performSegue(withIdentifier: "ToMainVC", sender: self)
+        })
+    }
+    
+}
